f8928731db9d3303d352cc15c083afc1b0918596
unknown <krajveer485@gmail.com>
2024-02-03 16:55:31 +0530

    remove ability to register

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes(['register' => false]);
+// Auth::routes();
 
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
